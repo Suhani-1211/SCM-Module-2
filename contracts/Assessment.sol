6555665c0b7b6d8468e8997c15cc8679e00c551f
Suhani-1211 <129710778+Suhani-1211@users.noreply.github.com>
2024-06-26 20:06:41 +0530

    Update Assessment.sol
    Added more functions

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,29 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event BalanceIncreased(uint256 amount);
+    event BalanceDecreased(uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
+
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
+    function deposit(uint256 _amount) public payable onlyOwner {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
 
         // perform transaction
         balance += _amount;
@@ -35,26 +38,60 @@ contract Assessment {
         emit Deposit(_amount);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
         uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
+        if (balance >= _withdrawAmount) {
+            // withdraw the given amount
+            balance -= _withdrawAmount;
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
+            // assert the balance is correct
+            assert(balance == (_previousBalance - _withdrawAmount));
+
+            // emit the event
+            emit Withdraw(_withdrawAmount);
+        } else {
+            // handle insufficient balance
+            emit Withdraw(0); // withdraw nothing if balance is insufficient
+        }
+    }
+
+    function transferOwnership(address payable _newOwner) public onlyOwner {
+        if (_newOwner != address(0)) {
+            emit OwnershipTransferred(owner, _newOwner);
+            owner = _newOwner;
+        } else {
+            // handle invalid new owner address
+            emit OwnershipTransferred(owner, owner); // no change in ownership
+        }
+    }
+
+    function increaseBalance(uint256 _amount) public onlyOwner {
+        uint _previousBalance = balance;
+
+        // increase balance
+        balance += _amount;
 
         // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+        assert(balance == (_previousBalance + _amount));
 
         // emit the event
-        emit Withdraw(_withdrawAmount);
+        emit BalanceIncreased(_amount);
+    }
+
+    function decreaseBalance(uint256 _amount) public onlyOwner {
+        uint _previousBalance = balance;
+        if (balance >= _amount) {
+            // decrease balance
+            balance -= _amount;
+
+            // assert the balance is correct
+            assert(balance == (_previousBalance - _amount));
+
+            // emit the event
+            emit BalanceDecreased(_amount);
+        } else {
+            // handle insufficient balance
+            emit BalanceDecreased(0); // decrease nothing if balance is insufficient
+        }
     }
 }
